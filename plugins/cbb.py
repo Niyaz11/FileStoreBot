93c1d89fdd0f0063c459a9b9abfb4c775639a479
Niyaz11 <145466262+Niyaz11@users.noreply.github.com>
2025-04-06 00:54:51 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,9 +1,9 @@
-#@Stelleron_Hunter
+# @Stelleron_Hunter
 
-from pyrogram import Client
+import os
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from bot import Bot
 from config import *
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from database.database import add_user, del_user, full_userbase, present_user
 
 START_MSG2 = os.environ.get(
@@ -14,45 +14,49 @@ START_MSG2 = os.environ.get(
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
+
     if data == "help":
         await query.message.edit_text(
             text=HELP_TXT.format(first=query.from_user.first_name),
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                    InlineKeyboardButton(" ʜᴏᴍᴇ", callback_data = "start"),
-                    InlineKeyboardButton(" ᴄʟᴏꜱᴇ", callback_data = "close")
+                        InlineKeyboardButton("ʜᴏᴍᴇ", callback_data="start"),
+                        InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data="close")
                     ]
                 ]
             )
         )
+
     elif data == "about":
         await query.message.edit_text(
             text=ABOUT_TXT.format(first=query.from_user.first_name),
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                    InlineKeyboardButton(" ʜᴏᴍᴇ", callback_data = "start"),
-                    InlineKeyboardButton(" ᴄʟᴏꜱᴇ", callback_data = "close")
+                        InlineKeyboardButton("ʜᴏᴍᴇ", callback_data="start"),
+                        InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data="close")
                     ]
                 ]
             )
         )
+
     elif data == "start":
-    await query.message.edit_text(
-        text=START_MSG2.format(first=query.from_user.first_name),
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
+        await query.message.edit_text(
+            text=START_MSG2.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
-                    InlineKeyboardButton(" ʜᴇʟᴘ", callback_data="help"),
-                    InlineKeyboardButton(" ᴀʙᴏᴜᴛ", callback_data="about")
+                    [
+                        InlineKeyboardButton("ʜᴇʟᴘ", callback_data="help"),
+                        InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="about")
+                    ]
                 ]
-            ]
+            )
         )
-    )
+
     elif data == "close":
         await query.message.delete()
         try:
